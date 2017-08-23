8b77ceef77c3c58ba787857479fe3c358e0009fe
BDLoneR <plumeria.in.sky@gmail.com>
2017-08-23 20:12:04 +0700

    lab02 + homework

--- lab02/MyApplication/app/src/main/java/kmitl/it/lab/myapplication/MainActivity.java
+++ lab02/MyApplication/app/src/main/java/kmitl/it/lab/myapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_constraintlayout_lab_09);
+        setContentView(R.layout.homework_02);
     }
 }
